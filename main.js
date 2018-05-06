b858272175ba89dc068646281e3872eb597aeda3
keyuanyuan <229497835@qq.com>
2018-05-06 16:06:21 +0800

    无缝轮播第一次提交

---
+++ main.js
@@ -0,0 +1,39 @@
+let n
+init()
+
+setInterval(() => {
+    makeLeave(getImage(n))
+        .one('transitionend', (e) => {
+            makeEnter($(e.currentTarget))
+        })
+    makeCurrent(getImage(n + 1))
+    n += 1
+}, 3000)
+
+function x(n) {
+    n = n % 3
+    if (n === 0) {
+        n = 3
+    }
+    return n
+}//返回1,2,3,1,2,3这样
+
+function getImage(n) {
+    return $(`.images>img:nth-child(${x(n)})`)
+}//ES6中反撇号模板字符串
+
+function init() {
+    n = 1
+    $(`.images>img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeLeave($node) {
+    return $node.removeClass('current enter').addClass('leave')
+}
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter')
+}
+function makeCurrent($node) {
+    return $node.removeClass('enter leava').addClass('current')
+}
